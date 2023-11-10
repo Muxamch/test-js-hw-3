814638c2c34136eb79837a47005f6276de9bf71f
Cemeachin Mihal <Muxamch@gmail.com>
2023-11-10 12:13:36 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,1 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  const arr = firstArray.concat(secondArray);
-  if (arr.length > maxLength) {
-    arr.length = maxLength;
-  }
-  return arr;
-}
+const getUsersWithFriend = (users, friendName) => { return users.filter((user) => user.friends.includes(friendName)); };
