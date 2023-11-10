a2f0e081a6f2c2ba5e1dfb621554a404ce937905
Cemeachin Mihal <Muxamch@gmail.com>
2023-11-10 12:14:06 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,4 @@
 
+function getTotalBalanceByGender(users, gender) { const totalBalance = users .filter((user) => user.gender === gender) .reduce((acc, user) => acc + user.balance, 0);
+
+return totalBalance; }
