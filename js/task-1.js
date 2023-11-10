450e431022ca5d483253903498a877a1a28815d5
Cemeachin Mihal <Muxamch@gmail.com>
2023-11-10 12:13:08 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,1 @@
-function isEnoughCapacity(products, containerSize) { let summProducts = 0; for (const product in products) { summProducts += products[product]; } return summProducts <= containerSize; }
+const getUserNames = (users) => { return users.map((u) => u.name); };
